f11fddea8a3ab2ac7d642a9e28043b857ca313f3
Phuong Tran <phuong.tran@saigontechnology.vn>
2016-03-10 14:14:51 +0700

    Added web project

---
+++ Framework/Web/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+        }
+    }
+}
